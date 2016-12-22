6b686917352923c1e14a330d3dfe3579c6fac1a9
Avital <avitalg91@gmail.com>
2016-12-22 17:21:15 +0200

    clean

--- test.R
+++ test.R
@@ -3,30 +3,29 @@ require(ggplot2)
 require(rworldmap)
 setwd("C:\\Users\\avital\\Documents\\R_Graph\\data mining course")
 
-#get dataset fro csv
+#get dataset from csv
 file<-"data.csv"
 Data<-read.csv(file, header=TRUE)
 DF<-Data[,c("time","latitude","longitude", "depth","mag", "id", "type", "gap")]
 mydata <- data.frame(DF)
 nrow(mydata)
 
+summary(data)
+
 #fix columns' type
 mydata$time<- strptime(as.character(mydata$time), format = "%Y-%m-%dT%H:%M:%S")
 mydata$id = as.character(mydata$id)
 
 #cleaning
-mydata<-mydata[mydata$type == 'earthquake',]        #only earthquake
+mydata<-mydata[mydata$type == 'earthquake',]        #only earthquakes
 mydata<-mydata[!duplicated(mydata$id),]             #unique id
-rownames(mydata)<-mydata$id                         #change row name to it's id
+rownames(mydata)<-mydata$id                         #the id will be the row name
 
 nrow(mydata)
 
 #add timestamp column 
 mydata$timestemp<-as.numeric(as.POSIXct(mydata$time, tz="GMT"))
-#longitude, latitude, depth, mag
-#plot 
-gapAmount<-mydata[mydata$gap>=180,]
-gapPres<-nrow(gapAmount)/nrow(mydata)             #Calculates the presentage of gaps over 180
+
 #map
 newmap <- getMap(resolution = "high")
 plot(newmap, xlim = c(-125, -100), ylim = c(25, 45), asp = 1, main="California Earthquakes")
